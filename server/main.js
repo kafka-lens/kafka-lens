d9584a7e9815a2a1fe4644f53d9ab8a41d250178
Harmon Huynh <harmon.huynh@outlook.com>
2019-02-09 17:36:57 -0800

    Removed unnecessary files from server dir. Created method for getting topic data from kafka server.

---
+++ server/main.js
@@ -0,0 +1,35 @@
+const { ipcMain } = require('electron');
+const kafka = require('kafka-node');
+
+function buildTopicObj(topic, partition, offset) {
+  return {
+    topic,
+    partition,
+    offset
+  };
+}
+
+function getCurrentOffset(topic, client) {
+  console.log('Doing getCurrentOffset');
+  const offset = new kafka.Offset(client);
+  return offset.fetchLatestOffsets([topic], (err, result) =>
+    Object.values(result[topic]).reduce((total, curr) => total + curr)
+  );
+}
+
+const getTopicData = (event, args) => {
+  const client = new kafka.KafkaClient({ kafkaHost: args });
+  const admin = new kafka.Admin(client);
+  admin.listTopics((err, topics) => {
+    if (err) console.error(err);
+    topics = topics[1].metadata;
+    const resultTopic = [];
+    Object.keys(topics).forEach(topic => {
+      const topicPartitions = Object.keys(topics[topic]).length;
+      resultTopic.push(buildTopicObj(topic, topicPartitions, getCurrentOffset(topic, client)));
+    });
+    return resultTopic;
+  });
+};
+
+module.exports = { getTopicData };
