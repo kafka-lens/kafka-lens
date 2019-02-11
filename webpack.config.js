c9d3a93e6f837331d4a0e60b0549d52c4182a927
Harmon Huynh <harmon.huynh@outlook.com>
2019-02-11 11:57:05 -0800

    Rearranged file structure

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, './client/src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'client/dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'client/index.html')
+    })
+  ]
+};
