909ece6ce2a4b82c61cef9838ce48ca8a8dd0052
Harmon Huynh <harmon.huynh@outlook.com>
2019-02-12 16:46:36 -0800

    Pull from develop

--- server/kafka/consumerApi.js
+++ server/kafka/consumerApi.js
@@ -4,26 +4,55 @@ const kafka = require('kafka-node');
 // const consumer = new kafka.Consumer(client, [{ topic: 'test1' }]);
 let testStream;
 
-const getMessagesFromTopic = (broker, topic, mainWindow) {
+const getLatestOffset = (kafkaHost, topic, partition) => {
+  const client = new kafka.KafkaClient({ kafkaHost });
+  const admin = new kafka.Admin(client);
+  const result = admin.getLatestOffset(client, topic, partition);
+  console.log('result from latest offset in consumer: ', result);
+  return result;
+};
+
+const getMessagesFromPartition = (
+  kafkaHost,
+  topic,
+  mainWindow,
+  offset = getLatestOffset(topic),
+  partition = 0
+) => {
   // Send back test data
   if (topic === 'test1' && broker === 'asdf') {
-    let testOffset = 45532
-    let testHighwater = 45702
+    let testOffset = 45532;
+    let testHighwater = 45702;
     testStream = setInterval(() => {
-      mainWindow.webContents.send('partition:getMessages', { topic: 'test1',
-      value: `Current date: ${Date.now()}`,
-      offset: testOffset,
-      partition: 0,
-      highWaterOffset: testHighwater,
-      key: null })
-    })
+      mainWindow.webContents.send('partition:getMessages', {
+        topic: 'test1',
+        value: `Current date: ${Date.now()}`,
+        offset: testOffset,
+        partition: 0,
+        highWaterOffset: testHighwater,
+        key: null,
+      });
+    });
     testOffset += 1;
     testHighwater += 1;
+  } else {
+    const client = new kafka.KafkaClient({ kafkaHost });
+    const payload = { topic, offset, partition };
+    const options = { encoding: 'utf8', keyEncoding: 'utf8' };
+    const consumerStream = new kafka.consumerStream(client, payload, options);
+
+    consumerStream.on('error', err => {
+      mainWindow.webContents.send('error:getMessage', {});
+    });
+
+    consumerStream.on('message', message => {
+      mainWindow.webContents.send('partition:getMessages', message);
+    });
   }
-}
+};
 
 const stopDataFlow = () => {
   clearInterval(testStream);
-}
+};
 
-module.exports={getMessagesFromTopic, stopDataFlow}
+module.exports = { getMessagesFromTopic: getMessagesFromPartition, stopDataFlow };
