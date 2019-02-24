a09a19ac8620c9eba31565fb52e1cdab028df989
jordanzobean <jordanbetzer@gmail.com>
2019-02-23 16:00:18 -0800

    refactored Topic.jsx and added tests for Topic and ParitionList.

--- client/src/app/components/Topic.jsx
+++ client/src/app/components/Topic.jsx
@@ -1,34 +1,23 @@
 import React from 'react';
 import '../css/Topic.scss';
 
-class Topic extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      topics: []
-    };
-
-    //bind methods here
-  }
-
-  render() {
-    return (
+const Topic = props => {
+  return (
+    <div>
       <div>
-        <div>
-          <span className="topic-name">{this.props.topicInfo.topic}</span> <br />
-          <span>Partitions: {this.props.topicInfo.partition}</span>
-        </div>
-        <button
-          className="view-button btn waves-effect waves-light"
-          id={this.props.id}
-          onClick={this.props.showPartitions}
-        >
-          View
-        </button>
-        <br />
+        <span className="topic-name">{props.topicInfo.topic}</span> <br />
+        <span>Partitions: {props.topicInfo.partition}</span>
       </div>
-    );
-  }
-}
+      <button
+        className="view-button btn waves-effect waves-light"
+        id={props.id}
+        onClick={props.showPartitions}
+      >
+        View
+      </button>
+      <br />
+    </div>
+  );
+};
 
 export default Topic;
