999441b81920a80c7a03ddcf3bc5233c900e01b3
Harmon Huynh <harmonh@gmail.com>
2019-02-25 12:26:43 -0800

    Squished bug in topic jsx component

--- client/src/app/components/Topic.jsx
+++ client/src/app/components/Topic.jsx
@@ -1,38 +1,31 @@
 import React from 'react';
 import '../css/Topic.scss';
 
-import PartitionList from '../components/PartitionList.jsx';
+import PartitionList from './PartitionList.jsx';
 
 class Topic extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      topics: []
+      topics: [],
     };
-    //bind methods here
+    // bind methods here
   }
 
   render() {
-
     return (
-
       <div className="topic-header">
-        <div id={this.props.id} onClick={this.props.showPartitions}> 
+        <div id={this.props.id} onClick={this.props.showPartitions}>
           {this.props.topicInfo.topic}
         </div>
-          {this.props.topicInfo.showPartitions === true ? <PartitionList showMessages={this.props.showMessages} topicInfo={this.props.topicInfo} /> : ""}
-
+        {this.props.topicInfo.showPartitions === true ? (
+          <PartitionList showMessages={this.props.showMessages} topicInfo={this.props.topicInfo} />
+        ) : (
+          ''
+        )}
       </div>
-      <button
-        className="view-button btn waves-effect waves-light"
-        id={props.id}
-        onClick={props.showPartitions}
-      >
-        View
-      </button>
-      <br />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Topic;
