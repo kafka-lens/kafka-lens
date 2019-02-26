a08745b8fbf19eb44dc3a86d5ed6e504f6761221
jordanzobean <jordanbetzer@gmail.com>
2019-02-26 11:59:55 -0800

    refactored App.jsx from class to functional component

--- client/src/app/containers/App.jsx
+++ client/src/app/containers/App.jsx
@@ -2,20 +2,8 @@ import React from 'react';
 
 import Main from './Main.jsx';
 
-// const App = props => {
-//   return <Main />;
-// };
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-
-    // bind methods here
-  }
-
-  render() {
-    return <Main />;
-  }
-}
+const App = () => {
+  return <Main />;
+};
 
 export default App;
