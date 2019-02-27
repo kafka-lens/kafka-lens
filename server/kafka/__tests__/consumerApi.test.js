3504c85ac66bffadda3578c2dd675fa41cdee6c7
jordanzobean <jordanbetzer@gmail.com>
2019-02-27 10:48:34 -0800

    moved tests to tests folder

---
+++ server/kafka/__tests__/consumerApi.test.js
@@ -0,0 +1,17 @@
+import consumerApi from '../consumerApi';
+
+describe('Testing consumerApi', () => {
+  const mainWindow = {
+    webContents: {
+      send: (message, data) => {
+        console.log(data);
+      },
+    },
+  };
+  describe('getLatestOffset should return a number', () => {
+    it('Should return a number', () =>
+      consumerApi.getLatestOffset('k2.tpw.made.industries:9092', 'swipes', 0).then(result => {
+        expect(result).toBeGreaterThanOrEqual(0);
+      }));
+  });
+});
