c1316013c56c60d9e7eb57e66af87bae90ff6e92
Angel Vega <angelvega85@gmail.com>
2019-07-08 17:40:13 -0700

    removed JSON parse

--- client/src/app/components/MessageInfo.jsx
+++ client/src/app/components/MessageInfo.jsx
@@ -6,7 +6,7 @@ const MessageInfo = props => {
     <div className="message-details">
         <h5>Last Message</h5>
         <p className="info-font-size">Topic: {props.lastMessage.topic}</p>
-        <p className="info-font-size">Message: {JSON.parse(props.lastMessage.value).message}</p>
+        <p className="info-font-size">Message: {props.lastMessage.value}</p>
         <p className="info-font-size">Timestamp: {props.lastMessage.timestamp}</p>
         <p className="info-font-size">Size: {props.lastMessage.size}</p>
     </div>
