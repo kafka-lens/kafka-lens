a716b07d10d51005a932b1910ff5e65f52c02db7
Brian Kwok <briankwok@Brians-MacBook-Pro.local>
2019-07-09 20:45:56 -0700

    brokerview almost complete

--- client/src/app/containers/Broker.jsx
+++ client/src/app/containers/Broker.jsx
@@ -1,32 +1,48 @@
 import React, { Component } from 'react';
-import BrokerView from '../components/BrokerView';
+import BrokerView from '../components/BrokerView.jsx';
 
 class Broker extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      brokersInfo: []
+      brokers: [
+        {
+          brokerId: 1,
+          brokerURI: 'localhost:test',
+          isAlive: true,
+          topics: [{ topicName: 'testName', newMessagesPerSecond: 15 }]
+        },
+        {
+          brokerId: 2,
+          brokerURI: 'localhost:test2',
+          isAlive: false,
+          topics: [
+            { topicName: 'testTopic1', newMessagesPerSecond: 15 },
+            { topicName: 'testTopic2', newMessagesPerSecond: 2340 }
+          ]
+        }
+      ]
     };
   }
+
+  /*
   // create method to parse through our data from the backend and set our new state
   componentDidMount() {
     // channel of listner to retain broker information from the backend and set state
     ipcRender.on('', (e, data) => {
       this.setState({
-        brokersInfo: data
+        brokers: data
       });
     });
   }
+  */
 
   render() {
     const arr = [];
-    for(let i = 0; i < this.state.brokersInfo.length; i += 1) {
-      arr.push(<BrokerView individualBroker={this.state.brokersInfo[i]} />)
-    return (
-      <div>
-        {arr} 
-      </div>
-    );
+    for (let i = 0; i < this.state.brokers.length; i += 1) {
+      arr.push(<BrokerView key={i} {...this.state.brokers[i]} />);
+    }
+    return <div>{arr}</div>;
   }
 }
 
