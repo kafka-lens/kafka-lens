2814c417d34b3f5949eaa1a65f42343f16befaa1
Brian Kwok <briankwok@Brians-MacBook-Pro.local>
2019-07-09 18:06:24 -0700

    brokerview

---
+++ client/src/app/components/BrokerTopic.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const BrokerTopic = props => {
+  return (
+    <div>
+        <h1>Topic Name: {props.topicName}</h1>
+        <h1>Msg Per Second: {props.newMessagesPerSecond}</h1>
+      </div>
+  );
+};
+
+export default BrokerTopic;
