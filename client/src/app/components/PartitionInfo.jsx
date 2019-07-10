8fabad47642fc300e0d0531fb71508a53f04f646
gp5yk <25909829+gipsykeys@users.noreply.github.com>
2019-07-09 19:19:58 -0700

    normalize messageCount to msgCount

--- client/src/app/components/PartitionInfo.jsx
+++ client/src/app/components/PartitionInfo.jsx
@@ -3,12 +3,12 @@ import '../css/PartitionInfo.scss';
 
 const PartitionInfo = props => {
   return (
-
     <div className="partition-details">
-        <h5>Partition: {props.partitionId}</h5>
-        <p className="info-font-size">Message Count: {props.infoBoxData.messageCount}</p>
-        <p className="info-font-size">Highwater Offset: {props.infoBoxData.highwaterOffset}</p>
-        <p className="info-font-size">Earliest Offset: {props.infoBoxData.earliestOffset}</p>
+      {console.log(props.infoBoxData.msgCount)}
+      <h5>Partition: {props.partitionId}</h5>
+      <p className="info-font-size">Message Count: {props.infoBoxData.msgCount}</p>
+      <p className="info-font-size">Highwater Offset: {props.infoBoxData.highwaterOffset}</p>
+      <p className="info-font-size">Earliest Offset: {props.infoBoxData.earliestOffset}</p>
     </div>
   );
 };
