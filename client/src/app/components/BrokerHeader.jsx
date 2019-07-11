bb6e165ee54f2deb898b4c5fab4241fb66b092a8
Brian Kwok <briankwok@Brians-MacBook-Pro.local>
2019-07-11 13:36:19 -0700

    broker page kinda styled

--- client/src/app/components/BrokerHeader.jsx
+++ client/src/app/components/BrokerHeader.jsx
@@ -5,7 +5,8 @@ const BrokerHeader = props => {
   return (
     <React.Fragment>
       <h1>Broker ID:{props.brokerID}</h1>
-      <h1>Broker URI:{props.brokerURI}</h1>
+      <h4>Broker URI:</h4>
+      <p>{props.brokerURI}</p>
     </React.Fragment>
   );
 };
