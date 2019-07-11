f0f9f95b00028ddbb83a2385b9713bfffb689bb9
Brian Kwok <briankwok@Brians-MacBook-Pro.local>
2019-07-11 13:36:19 -0700

    broker page kinda styled

--- client/src/app/components/Navbar.jsx
+++ client/src/app/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 function Navbar({ routes }) {
   const NavLinks = routes.map((e, index) => (
     <li key={index}>
-      <NavLink to={e.link}>{e.text}</NavLink>
+      <NavLink style={{ color: '#f7f7f7'}}to={e.link}>{e.text}</NavLink>
     </li>
   ));
   return <ul style={{ margin : 0}}>{NavLinks}</ul>;
