ed27032ed1dce0c59ac6ca044aa0c7626d3a6e4a
Brian Kwok <briankwok@Brians-MacBook-Pro.local>
2019-07-10 17:56:19 -0700

    broker and topic nav bar

--- client/src/app/components/Navbar.jsx
+++ client/src/app/components/Navbar.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Navbar({ routes }) {
-  console.log(`routes`, routes)
   const NavLinks = routes.map((e, index) => (
     <li key={index}>
       <NavLink to={e.link}>{e.text}</NavLink>
     </li>
   ));
-  console.log(NavLinks)
   return <ul>{NavLinks}</ul>;
 }
 export default Navbar;
