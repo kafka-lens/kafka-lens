ff83bd9fdfc6a1729a9681ac061f7921a91881ec
Angel Vega <angelvega85@gmail.com>
2019-07-11 11:01:57 -0700

    newMessagesPerSecond

--- client/src/app/components/BrokerTopic.jsx
+++ client/src/app/components/BrokerTopic.jsx
@@ -5,7 +5,7 @@ const BrokerTopic = props => {
   return (
     <div>
         <h1>Topic Name: {props.topicName}</h1>
-        <h1>Msg Per Second: {props.messagesPerSecond}</h1>
+        <h1>Msg Per Second: {props.newMessagesPerSecond}</h1>
       </div>
   );
 };
