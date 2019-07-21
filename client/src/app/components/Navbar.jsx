c7503098deaac9581a046b1fb5a72c4f4a1e1c6a
Brian Kwok <briankwok@Brians-MacBook-Pro.local>
2019-07-21 15:18:14 -0700

    restart connection pagedone

--- client/src/app/components/Navbar.jsx
+++ client/src/app/components/Navbar.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-
-function Navbar({ routes }) {
+function Navbar({ routes, restartConnectionPage }) {
   const NavLinks = routes.map((e, index) => (
     <li key={index}>
-      <NavLink style={{ color: '#f7f7f7', fontSize: 18}}to={e.link}>{e.text}</NavLink>
+      <NavLink style={{ color: '#f7f7f7', fontSize: 18 }} to={e.link}>
+        {e.text}
+      </NavLink>
     </li>
   ));
-  return <ul style={{ margin : 0}}>{NavLinks}</ul>;
+
+  NavLinks.push(
+    <li key={`connection page`} >
+      <NavLink onClick ={restartConnectionPage} style={{ color: '#f7f7f7', fontSize: 18 }} to="/connectionpage">
+        Connection Page 
+      </NavLink>
+    </li>
+  );
+  return <ul style={{ margin: 0 }}>{NavLinks}</ul>;
 }
 export default Navbar;
