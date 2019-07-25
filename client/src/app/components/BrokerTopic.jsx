98c970b55cfb2b70525a29acb97dd4fe95d1d66b
Angel Vega <angelvega85@gmail.com>
2019-07-25 16:31:05 -0700

    Fixed logic bug in Broker Topic

--- client/src/app/components/BrokerTopic.jsx
+++ client/src/app/components/BrokerTopic.jsx
@@ -5,14 +5,14 @@ const BrokerTopic = ({ topicName, isLeader, newMessagesPerSecond }) => {
   return (
     <div>
       <p>
-        <b>Topic Name:</b> {topicName}
+        <b>Topic Name: </b> {topicName}
       </p>
-      {isLeader && typeof newMessagesPerSecond !== 'number' ? (
-        <p>
-          <b>Msg Per Second:</b> {newMessagesPerSecond || 'Follower'}
-        </p>
-      ) : (
+      {isLeader && typeof(newMessagesPerSecond) !== 'number' ? (
         <LoadingData />
+      ) : (
+      <p>
+        <b>Msg Per Second: </b> {!isLeader ? 'Follower' : newMessagesPerSecond}
+      </p>
       )}
     </div>
   );
