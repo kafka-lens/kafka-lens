92453d568fd1128bd010887ec79b6c66b746084f
Angel Vega <angelvega85@gmail.com>
2019-08-05 02:00:12 -0700

    Styling to the broker view nd connection page

--- client/src/app/components/BrokerTopicsView.jsx
+++ client/src/app/components/BrokerTopicsView.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import BrokerTopic from './BrokerTopic.jsx';
 
 const BrokerTopicsView = ({ topics }) => {
+  let totalNewMessagesPerSecond = 0;
   const brokerTopics = [];
   for (let i = 0; i < topics.length; i += 1) {
+    totalNewMessagesPerSecond += topics[i].newMessagesPerSecond;
     brokerTopics.push(
       <BrokerTopic
         key={i}
+        className="broker-topic"
         topicName={topics[i].topicName}
         newMessagesPerSecond={topics[i].newMessagesPerSecond}
         isLeader={topics[i].isLeader}
@@ -14,7 +17,15 @@ const BrokerTopicsView = ({ topics }) => {
     );
   }
 
-  return <div>{brokerTopics}</div>;
+  return (
+  <div className="broker-topic-view">
+    <p className="broker-topic-view-bold">Total Messages Per Second: {isNaN(totalNewMessagesPerSecond) ? 'Calculating': totalNewMessagesPerSecond}</p>
+    <p className="broker-topic-view-bold">Messages Per Second by Topic</p>
+    <div className="broker-topics-inner-container">
+      {brokerTopics}
+    </div>
+  
+  </div>);
 };
 
 export default BrokerTopicsView;
