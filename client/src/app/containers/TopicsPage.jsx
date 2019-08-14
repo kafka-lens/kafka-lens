b1de7525b1ba8f597974e21d2efe59c7d582c95f
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-08-14 14:54:31 -0700

    create TopicsPage.jsx

---
+++ client/src/app/containers/TopicsPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { getState } from '../state/state';
+
+function TopicsPage(props) {
+  const [state, dispatch] = getState();
+
+  return (
+    <div>
+      {state.topics.topics}
+      <button onClick={() => dispatch({
+        type: 'SET_TOPICS',
+        payload: [1,2,3],
+      })}> Set Topics</button>
+    </div>
+  )
+}
