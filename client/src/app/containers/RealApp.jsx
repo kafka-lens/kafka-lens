f5137dc697e3cb538f3a6451a7068e281f949a1b
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-08-14 14:30:07 -0700

    create RealApp entrypoint component

---
+++ client/src/app/containers/RealApp.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { StateProvider } from '../state/state';
+import { brokerReducer, initialState as brokerReducerInitialState } from '../state/brokerReducer';
+import { topicReducer , initialState as topicReducerInitialState } from '../state/topicReducer';
+
+const App = () => {
+  const initialState = {
+    topics: topicReducerInitialState,
+    broker: brokerReducerInitialState,
+  }
+
+  const mainReducer = ({topics, brokers}, action) => ({
+    topics: topicReducer(topics, action),
+    brokers: brokerReducer(brokers, action),
+  });
+
+  return (
+    <StateProvider initialState={initialState} reducer={mainReducer}>
+      // App content ...
+    </StateProvider>
+  );
+}
