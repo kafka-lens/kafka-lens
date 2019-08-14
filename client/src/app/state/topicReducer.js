b7132e8389d129d9cae0c82027667acb1a1f89af
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-08-14 14:52:30 -0700

    add topics state and SET_TOPICS action type

--- client/src/app/state/topicReducer.js
+++ client/src/app/state/topicReducer.js
@@ -1,10 +1,14 @@
 export const initialState = {
-  //...
+  topics: [],
 };
 
 function topicReducer(state=initialState, action) {
   switch (action.type) {
-    //...
+    case 'SET_TOPICS':
+      return {
+        ...state,
+        topics: action.payload,
+      }
     default:
       return state;
   }
