a9a954ee15b40edc29b87a3fa585ac623c47449e
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-08-15 00:40:29 -0700

    add SET_TOPICS and SHOW_PARTITIONS events in topicReducer

--- client/src/app/state/topicReducer.js
+++ client/src/app/state/topicReducer.js
@@ -2,14 +2,17 @@ export const initialState = {
   topics: [],
 };
 
-export const topicReducer = (state=initialState, action) => {
+export const topicReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'SET_TOPICS':
-      return {
-        ...state,
-        topics: action.payload,
-      }
-    default:
-      return state;
+  case 'SET_TOPICS':
+    return {
+      ...state,
+      topics: action.payload,
+    };
+  case 'SHOW_PARTITION':
+
+    return action.payload;
+  default:
+    return state;
   }
 };
