cd360343c2707f1f15aa53b8ede0e228347ce002
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-08-16 13:10:06 -0700

    add defaults to startLocalCluster.sh

--- startLocalCluster.sh
+++ startLocalCluster.sh
@@ -4,7 +4,10 @@
 # ./bin/kafka-server-start.sh config/server-2.properties
 # ./bin/kafka-console-producer.sh --broker-list localhost:9092 --topic
 
-cd $1
+kafka=${1:-~/kafka_2.12-2.3.0/}
+topic=${2:-first}
+cd "$kafka" || exit
+echo in folder: $(pwd)
 # BROKER1="${$3:-server}"
 # BROKER2="${$4:-server-1}"
 # BROKER3="${$5:-server-2}"
@@ -13,4 +16,4 @@ gnome-terminal --tab --title="zookeeper" --command="bash -c './bin/zookeeper-ser
 --tab --title="broker 1" --command="bash -c './bin/kafka-server-start.sh config/${3:-server}.properties; $SHELL'" \
 --tab --title="broker 2" --command="bash -c './bin/kafka-server-start.sh config/${4:-server-1}.properties; $SHELL'" \
 --tab --title="broker 3" --command="bash -c './bin/kafka-server-start.sh config/${5:-server-2}.properties; $SHELL'" \
---tab --title="producer $2" --command="bash -c './bin/kafka-console-producer.sh --broker-list localhost:9092 --topic $2; $SHELL'" 
+--tab --title="producer ${topic}" --command="bash -c './bin/kafka-console-producer.sh --broker-list localhost:9092 --topic ${topic}; $SHELL'"
