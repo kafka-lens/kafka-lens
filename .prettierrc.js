5c32281e6af5a5dbfdb1fa753e5bf51017f14dca
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-08-15 18:21:00 -0700

    create prettierrc config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,5 @@
-{
-  "printWidth": 100,
-  "singleQuote": true,
-  "comma-dangle": [
-    "error",
-    {
-      "arrays": "only-multiline",
-      "objects": "only-multiline",
-      "imports": "never",
-      "exports": "never",
-      "functions": "ignore"
-    }
-  ]
-}
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+};
