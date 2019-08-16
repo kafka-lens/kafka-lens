1ddbdf90b46bae544df3c72117c0c06d296aea50
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-08-16 15:14:17 -0700

    add default topic creation

--- startLocalCluster.sh
+++ startLocalCluster.sh
@@ -4,9 +4,9 @@
 # ./bin/kafka-server-start.sh config/server-2.properties
 # ./bin/kafka-console-producer.sh --broker-list localhost:9092 --topic
 
-kafka=${1:-~/kafka_2.12-2.3.0/}
+KAFKA=${1:-~/kafka_2.12-2.3.0/}
 topic=${2:-first}
-cd "$kafka" || exit
+cd "$KAFKA" || exit
 echo in folder: $(pwd)
 # BROKER1="${$3:-server}"
 # BROKER2="${$4:-server-1}"
@@ -16,4 +16,6 @@ gnome-terminal --tab --title="zookeeper" --command="bash -c './bin/zookeeper-ser
 --tab --title="broker 1" --command="bash -c './bin/kafka-server-start.sh config/${3:-server}.properties; $SHELL'" \
 --tab --title="broker 2" --command="bash -c './bin/kafka-server-start.sh config/${4:-server-1}.properties; $SHELL'" \
 --tab --title="broker 3" --command="bash -c './bin/kafka-server-start.sh config/${5:-server-2}.properties; $SHELL'" \
+--tab --title="create topic ${topic}" --command="bash -c './bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 3 --partitions 3 --topic ${topic}'" \
+--tab --title="consumer ${topic}" --command="bash -c './bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --from-beginning --topic ${topic}'" \
 --tab --title="producer ${topic}" --command="bash -c './bin/kafka-console-producer.sh --broker-list localhost:9092 --topic ${topic}; $SHELL'"
