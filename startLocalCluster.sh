a14d847cf969e6b9a8b46cafa1587999e147cf5e
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-08-16 15:16:29 -0700

    refactor startLocalCluster

--- startLocalCluster.sh
+++ startLocalCluster.sh
@@ -4,18 +4,19 @@
 # ./bin/kafka-server-start.sh config/server-2.properties
 # ./bin/kafka-console-producer.sh --broker-list localhost:9092 --topic
 
-KAFKA=${1:-~/kafka_2.12-2.3.0/}
-topic=${2:-first}
+topic=${1:-first}
+KAFKA=${2:-~/kafka_2.12-2.3.0/}
+BROKER1="${$3:-server}"
+BROKER2="${$4:-server-1}"
+BROKER3="${$5:-server-2}"
+
 cd "$KAFKA" || exit
 echo in folder: $(pwd)
-# BROKER1="${$3:-server}"
-# BROKER2="${$4:-server-1}"
-# BROKER3="${$5:-server-2}"
 
 gnome-terminal --tab --title="zookeeper" --command="bash -c './bin/zookeeper-server-start.sh config/zookeeper.properties; $SHELL'" \
---tab --title="broker 1" --command="bash -c './bin/kafka-server-start.sh config/${3:-server}.properties; $SHELL'" \
---tab --title="broker 2" --command="bash -c './bin/kafka-server-start.sh config/${4:-server-1}.properties; $SHELL'" \
---tab --title="broker 3" --command="bash -c './bin/kafka-server-start.sh config/${5:-server-2}.properties; $SHELL'" \
+--tab --title="broker 1" --command="bash -c './bin/kafka-server-start.sh config/${BROKER1}.properties; $SHELL'" \
+--tab --title="broker 2" --command="bash -c './bin/kafka-server-start.sh config/${BROKER2}.properties; $SHELL'" \
+--tab --title="broker 3" --command="bash -c './bin/kafka-server-start.sh config/${BROKER3}.properties; $SHELL'" \
 --tab --title="create topic ${topic}" --command="bash -c './bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 3 --partitions 3 --topic ${topic}'" \
 --tab --title="consumer ${topic}" --command="bash -c './bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --from-beginning --topic ${topic}'" \
 --tab --title="producer ${topic}" --command="bash -c './bin/kafka-console-producer.sh --broker-list localhost:9092 --topic ${topic}; $SHELL'"
