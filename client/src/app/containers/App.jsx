0f93eb35efa957318fc0bdce83b5e656b2538e54
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-08-16 02:44:43 -0700

    comment out dummy container TopicsPage from App

--- client/src/app/containers/App.jsx
+++ client/src/app/containers/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
   return (
     <StateProvider initialState={initialState} reducer={mainReducer}>
       <Main />
-      <TopicsPage />
+      {/* <TopicsPage /> */}
     </StateProvider>
   );
 };
