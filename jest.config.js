2c2a530e5f38638cd42c4323329e3fb65bf65470
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-08-15 17:56:03 -0700

    prettify jest.config

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,8 @@ module.exports = {
   ],
   moduleNameMapper: {
     '\\.(css|less|scss)$': '<rootDir>/client/src/app/__mocks__/styleMock.js',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/client/src/app/__mocks__/assetsTransformer.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/client/src/app/__mocks__/assetsTransformer.js',
     electron: '<rootDir>/client/src/app/__mocks__/electron.js',
   },
   moduleDirectories: [
