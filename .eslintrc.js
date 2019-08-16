2cd70e240c9e7ddb57ac0cc244c35b9196e32840
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-08-15 18:21:17 -0700

    prettify .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,48 +3,51 @@ module.exports = {
   extends: [
     'airbnb',
     'plugin:react/recommended',
+    'prettier',
     // 'plugin:import/react',
   ],
+  plugins: ['prettier'],
   env: {
     browser: true,
     node: true,
     jest: true,
   },
   rules: {
+    'prettier/prettier': 'error',
     'max-len': ['warn', { code: 100 }],
     semi: ['error', 'always'],
     camelcase: 'error',
-    indent: ['error', 2],
+    indent: ['error', 2, { SwitchCase: 1 }],
     'prefer-const': [
       'error',
       {
-        'destructuring': 'any',
-        'ignoreReadBeforeAssign': false
-      }
+        destructuring: 'any',
+        ignoreReadBeforeAssign: false,
+      },
     ],
     'no-use-before-define': [
       'error',
       {
         functions: false,
-        classes: true
-      }
+        classes: true,
+      },
     ],
-    'no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     'no-console': 'off',
-    'no-plusplus': ["error", { "allowForLoopAfterthoughts": true }],
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
   },
   settings: {
     'import/resolver': {
       node: {},
       webpack: {},
-    }
+    },
   },
   overrides: [
     {
       files: ['*.test.jsx'],
       rules: {
         'react/jsx-props-no-spreading': 'off',
-      }
-    }
-  ]
+      },
+    },
+  ],
 };
