2506629081d33e37804eb8c3a591dab7c78a0e27
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-08-31 11:33:29 -0700

    add electron target to @babel/preset-env

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,12 @@
-module.exports = function(api) {
+const electronVersion = require('electron/package.json').version;
+
+module.exports = api => {
   api.cache(true);
 
-  const presets = ['@babel/preset-env', '@babel/preset-react'];
+  const presets = [
+    ['@babel/preset-env', { targets: { electron: electronVersion } }],
+    '@babel/preset-react',
+  ];
   const plugins = [];
 
   return {
