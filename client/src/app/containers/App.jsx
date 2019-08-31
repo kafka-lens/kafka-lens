304390591ad48aa3d9eb5aaa7c818a5dffa7b58c
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-08-31 13:38:32 -0700

    enable hot reloading for App, comment out hooks code

--- client/src/app/containers/App.jsx
+++ client/src/app/containers/App.jsx
@@ -1,29 +1,31 @@
+import { hot } from 'react-hot-loader';
 import React from 'react';
 
 import Main from './Main';
 // import HooksPage from './HooksPage';
 
-import { StateProvider } from '../state/state';
-import { brokerReducer, initialState as brokerReducerInitialState } from '../state/brokerReducer';
-import { topicReducer, initialState as topicReducerInitialState } from '../state/topicReducer';
+// import { StateProvider } from '../state/state';
+// import { brokerReducer, initialState as brokerReducerInitialState } from '../state/brokerReducer';
+// import { topicReducer, initialState as topicReducerInitialState } from '../state/topicReducer';
 
 const App = () => {
-  const initialState = {
-    topics: topicReducerInitialState,
-    brokers: brokerReducerInitialState,
-  };
+  // const initialState = {
+  //   topics: topicReducerInitialState,
+  //   brokers: brokerReducerInitialState,
+  // };
 
-  const mainReducer = ({ topics, brokers }, action) => ({
-    topics: topicReducer(topics, action),
-    brokers: brokerReducer(brokers, action),
-  });
+  // const mainReducer = ({ topics, brokers }, action) => ({
+  //   topics: topicReducer(topics, action),
+  //   brokers: brokerReducer(brokers, action),
+  // });
 
   return (
-    <StateProvider initialState={initialState} reducer={mainReducer}>
-      <Main />
-      {/* <HooksPage /> */}
-    </StateProvider>
+    // <StateProvider initialState={initialState} reducer={mainReducer}>
+    <Main />
+    // {/* <HooksPage /> */}
+    // </StateProvider>
   );
 };
 
-export default App;
+// export default App;
+export default hot(module)(App);
