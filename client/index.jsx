188e6eb27362d02f4515a613e805e9852aea02a0
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-08-31 13:37:52 -0700

    import react before react-dom for index.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,5 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './src/app/containers/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
