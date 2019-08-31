0f1a89080de52109b4a1e8a5b0a5fcc0106b8bfe
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-08-31 11:50:56 -0700

    only use react-hot-loader/babel plugin if in dev mode

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,16 @@
 const electronVersion = require('electron/package.json').version;
 
+const developmentEnvironments = ['development', 'test'];
+
 module.exports = api => {
   api.cache(true);
+  const isDevelopment = api.env(developmentEnvironments);
 
   const presets = [
     ['@babel/preset-env', { targets: { electron: electronVersion } }],
     '@babel/preset-react',
   ];
-  const plugins = [];
+  const plugins = isDevelopment ? ['react-hot-loader/babel'] : [];
 
   return {
     presets,
