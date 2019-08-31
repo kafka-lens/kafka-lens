8b4209772cbb98356c1ebba5180afbee0597b033
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-08-31 15:58:52 -0700

    enable RHL in index.jsx using AppContainer

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import { render } from 'react-dom';
 import App from './src/app/containers/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+render(
+  <AppContainer>
+    <App />
+  </AppContainer>,
+  document.getElementById('app'),
+);
+
+if (module.hot) {
+  module.hot.accept('./src/app/containers/App', () => {
+    // eslint-disable-next-line global-require
+    const NextRoot = require('./src/app/containers/App').default;
+    render(
+      <AppContainer>
+        <NextRoot />
+      </AppContainer>,
+      document.getElementById('app'),
+    );
+  });
+}
