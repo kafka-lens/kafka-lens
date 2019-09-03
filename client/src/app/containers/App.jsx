542601d6ed6941b04266bd1b4b3b24228c336e68
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-09-03 11:07:56 -0700

    replace AppContainer with hot api

--- client/src/app/containers/App.jsx
+++ client/src/app/containers/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { hot } from 'react-hot-loader';
 import Main from './Main';
 // import HooksPage from './HooksPage';
 
@@ -26,4 +26,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default hot(module)(App);
