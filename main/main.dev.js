a38ffb792ac4ad5140cdf02bb668e833dbd43cfa
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-09-04 08:48:02 -0700

    remove hardcoded DEBUG env variable in main.dev

--- main/main.dev.js
+++ main/main.dev.js
@@ -9,8 +9,6 @@ const consumerApi = require('./kafka/consumerApi');
 const brokerApi = require('./kafka/brokerApi');
 const logger = require('./utils/logger');
 
-process.env.DEBUG = false;
-
 // * Disable error dialogs by overriding
 // * FIX: https://goo.gl/YsDdsS
 dialog.showErrorBox = (title, content) => {
